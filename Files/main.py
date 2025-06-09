5f5bb583c3c1caf07ebe4f3aee35018462a1fe85
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-06-09 23:58:42 +0530

    Files

--- Files/main.py
+++ Files/main.py
@@ -26,6 +26,8 @@ while True:
                 h, w, c = img.shape
                 cx, cy = int(lm.x*w), int(lm.y*h)
                 print(id, cx, cy)
+                if id == 0:
+                    cv2.circle(img, (cx, cy), 5, (255, 0,255, cv2.FILLED))
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS) #for lining the hands
     
     cTime = time.time() #will give us current time
