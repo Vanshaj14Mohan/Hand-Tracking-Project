8bfad515341e36281d0e9196fe3bcd43e762c69e
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-06-09 21:35:03 +0530

    Files

--- Files/main.py
+++ Files/main.py
@@ -6,12 +6,18 @@ cap = cv2.VideoCapture(0)
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
 
 while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
-    print(results.multi_hand_landmarks)
+    #print(results.multi_hand_landmarks)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            # mpDraw.draw_landmarks(img, handLms) normal just points no lines on hands
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS) #for lining the hands
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
